1ab8bc0e4ee617cefe91e948ff8c7df33179f339
ncmbadmin <dinh.thuy.duong@nifty.co.jp>
2016-06-15 17:15:39 +0900

    first version

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is quickstart project for Android push Nifty cloud mobile backend
+
+## 環境
+
+    Android SDK 2.2.2
+
+## Reference
+
+  - [Document](http://mb.cloud.nifty.com/doc/current/index.html)
+  - [Android push document](http://mb.cloud.nifty.com/doc/current/push/basic_usage_android.html)
---
+++ app/src/main/java/mbaas/com/nifty/ncmbpushquickstart/MainActivity.java
@@ -0,0 +1,103 @@
+package mbaas.com.nifty.ncmbpushquickstart;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.nifty.cloud.mb.core.DoneCallback;
+import com.nifty.cloud.mb.core.FindCallback;
+import com.nifty.cloud.mb.core.NCMB;
+import com.nifty.cloud.mb.core.NCMBException;
+import com.nifty.cloud.mb.core.NCMBInstallation;
+import com.nifty.cloud.mb.core.NCMBQuery;
+
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //**************** APIキーの設定とSDKの初期化 **********************
+        NCMB.initialize(this, "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY");
+
+        final NCMBInstallation installation = NCMBInstallation.getCurrentInstallation();
+
+        //GCMからRegistrationIdを取得しinstallationに設定する
+        installation.getRegistrationIdInBackground("ANDROID_SENDER_ID", new DoneCallback() {
+            //installation.getRegistrationIdInBackground("senderId", new DoneCallback() {
+                @Override
+            public void done(NCMBException e) {
+                if (e == null) {
+                    installation.saveInBackground(new DoneCallback() {
+                        @Override
+                        public void done(NCMBException e) {
+                            if (e == null) {
+                                //保存成功
+                            } else if (NCMBException.DUPLICATE_VALUE.equals(e.getCode())) {
+                                //保存失敗 : registrationID重複
+                                updateInstallation(installation);
+                            } else {
+                                //保存失敗 : その他
+                            }
+                        }
+                    });
+                } else {
+                    //ID取得失敗
+                }
+            }
+        });
+
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public static void updateInstallation(final NCMBInstallation installation) {
+
+        //installationクラスを検索するクエリの作成
+        NCMBQuery<NCMBInstallation> query = NCMBInstallation.getQuery();
+
+        //同じRegistration IDをdeviceTokenフィールドに持つ端末情報を検索する
+        query.whereEqualTo("deviceToken", installation.getDeviceToken());
+
+        //データストアの検索を実行
+        query.findInBackground(new FindCallback<NCMBInstallation>() {
+            @Override
+            public void done(List<NCMBInstallation> results, NCMBException e) {
+
+                //検索された端末情報のobjectIdを設定
+                installation.setObjectId(results.get(0).getObjectId());
+
+                //端末情報を更新する
+                installation.saveInBackground();
+            }
+        });
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
